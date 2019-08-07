fd11418a1c3f54c7405ec0daa0bbd67ea679c698
Paulina Cortez <paulina.a.cortez@gmail.com>
2019-08-06 18:54:06 -0700

    Updates

---
+++ precache-manifest.fae0280d5c69f51621339c9791f1f45b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e9bb2fb9232361ce2ddc1beea72df9",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "98217a2e9e6eb14375a1",
+    "url": "/clicky-game/static/css/main.a8095a40.chunk.css"
+  },
+  {
+    "revision": "d6e0d9bc8c1204b52360",
+    "url": "/clicky-game/static/js/2.b05ca7b9.chunk.js"
+  },
+  {
+    "revision": "98217a2e9e6eb14375a1",
+    "url": "/clicky-game/static/js/main.8aa6819e.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
